190a010d340631d5b44c97f439cd1b1bf7054153
karlstomberg <karlstom@udel.edu>
2019-12-01 15:04:12 -0500

    Added error-handling services

---
+++ src/app/routes/routes.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+
+
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class RoutesService {
+  constructor(private http: HttpClient) { }
+}
