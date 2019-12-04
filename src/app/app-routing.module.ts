7b1d7d903b846117b0a700ffaf5ec1a41728c113
Alexandra Hurst <ahurst@udel.edu>
2019-12-03 20:02:51 -0500

    Add navigation to/from user profile page, but only if logged in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RoutesComponent } from './routes/routes.component';
 import { BuyComponent } from './buy/buy.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AuthGuardService } from './auth-guard.service';
 
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'routes', component: RoutesComponent},
+  {path: 'profile', component: ProfileComponent, canActivate: [AuthGuardService]},
   {path: 'routes/purchase', component: BuyComponent}
 ];
 
